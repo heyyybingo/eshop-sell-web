ebfc16eca8a19ced679854dd7be20c391323a787
heyyybingo <283385508@qq.com>
2021-01-09 21:56:15 +0800

    ss

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function ({ $axios, redirect }) {
     $axios.setBaseURL("/api")
     $axios.onRequest(config => {
-        console.log('Making request to ' + config)
+        console.log('Making request to ',config)
       })
     $axios.onError(error => {
       if (error.response.status === 500) {
